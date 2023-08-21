062371ab85c347ea2fa56af7163ad3f07a06d72b
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-08-21 12:52:02 -0400

    bd commit

--- lib/db/migrations/env.py
+++ lib/db/migrations/env.py
@@ -18,7 +18,8 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
-
+from models import Base
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
