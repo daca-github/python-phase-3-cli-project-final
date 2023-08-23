e525b675ed9f2b0ed91e94f3a0df1eeafc6a91d8
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-08-23 14:20:18 -0400

    Added get_user function to ask for the username

--- lib/cli.py
+++ lib/cli.py
@@ -2,6 +2,11 @@ import click
 from db.models import User, Car
 from db.database import SessionLocal as Session
 
+def get_user(session):
+    username = click.prompt('Please enter your username')
+    user = session.query(User).filter_by(username=username).first()
+    return user
+
 @click.group()
 def main():
     """CarBud: Your Virtual Dealership"""
@@ -9,35 +14,56 @@ def main():
 
 @main.command()
 def menu():
-    click.echo('Welcome to CarBud!')
-    click.echo('1. Used Cars')
-    click.echo('2. New Cars')
-    click.echo('3. Search by criteria')
-    click.echo('4. Exit')
+    session = Session()
+    user = get_user(session)
+    session.close()
+
+    if user:
+        click.echo(f'Welcome, {user.username}!')
+    else:
+        click.echo('User not found. Please register.')
+
+    click.echo('1. Display Available Cars')
+    click.echo('2. Search by criteria')
+    click.echo('3. Exit')
 
     choice = click.prompt('Please enter your choice', type=int)
 
     if choice == 1:
-        display_cars('Used')
+        display_available_cars()
     elif choice == 2:
-        display_cars('New')
-    elif choice == 3:
         search_by_criteria()
-    elif choice == 4:
+    elif choice == 3:
         exit()
     else:
         click.echo('Invalid choice. Please try again.')
 
-def display_cars(make):
-    click.echo(f'Displaying {make} cars...')
+def display_available_cars():
+    session = Session()
+    available_cars = session.query(Car).filter_by(status='Available').all()
+    session.close()
 
-@main.command()
+    if available_cars:
+        click.echo('Available Cars:')
+        for car in available_cars:
+            click.echo(f'{car.make} {car.model}, Year: {car.year}, Price: {car.price}')
+    else:
+        click.echo('No available cars at the moment.')
+
 def search_by_criteria():
+    session = Session()
     model = click.prompt('Enter the model')
     make = click.prompt('Enter the make')
     
-    click.echo(f'Searching for cars with model: {model} and make: {make}')
+    cars = session.query(Car).filter_by(model=model, make=make).all()
+    session.close()
 
+    if cars:
+        click.echo(f'Found {len(cars)} cars matching your criteria:')
+        for car in cars:
+            click.echo(f'{car.make} {car.model}, Year: {car.year}, Price: {car.price}')
+    else:
+        click.echo('No cars matching the criteria.')
 
 if __name__ == '__main__':
     menu()
